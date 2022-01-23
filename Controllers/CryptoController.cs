498fe2f71f68974906fd45b14d091b2ee3f06877
Sérgio Roberto Garcia Rodrigues Filho <sergiogarciabr@hotmail.com>
2022-01-23 15:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CryptoController.cs
@@ -0,0 +1,32 @@
+﻿using LT.Dominio.Infra;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crypto_helper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult ValidarCriptografia(string chave, string dadoEntrada, bool criptografar = false, bool descriptografar = false)
+        {
+            try
+            {
+                if (criptografar)
+                    return Ok(CriptografiaDados.Criptografar(chave, dadoEntrada));
+                if (descriptografar)
+                    return Ok(CriptografiaDados.Descriptografar(chave, dadoEntrada));
+                if (criptografar && descriptografar)
+                    return BadRequest("Escolha apenas uma das opções");
+                else
+                return BadRequest("Deve ser escolhido se é para criptografar ou descriptografar");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ Infra/CriptografiaDados.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LT.Dominio.Infra
+{
+    public static class CriptografiaDados
+    {
+        public static string Criptografar(string chave, string textoLimpo)
+        {
+            string chaveCriptografia = chave;
+            byte[] bytesLimpos = Encoding.Unicode.GetBytes(textoLimpo);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(chaveCriptografia, 
+                    new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                
+                using (MemoryStream streamMemoria = new MemoryStream())
+                {
+                    using (CryptoStream streamCriptografia = new CryptoStream(streamMemoria, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        streamCriptografia.Write(bytesLimpos, 0, bytesLimpos.Length);
+                        streamCriptografia.Close();
+                    }
+                    textoLimpo = Convert.ToBase64String(streamMemoria.ToArray());
+                }
+            }
+            return textoLimpo;
+        }
+        public static string Descriptografar(string chave, string textoCriptografado)
+        {
+            string chaveCriptografia = chave;
+            textoCriptografado = textoCriptografado.Replace(" ", "+");
+            byte[] criptografiaBytes = Convert.FromBase64String(textoCriptografado);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(chaveCriptografia, 
+                    new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream streamMemoria = new MemoryStream())
+                {
+                    using (CryptoStream streamCriptografia = new CryptoStream(streamMemoria, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        streamCriptografia.Write(criptografiaBytes, 0, criptografiaBytes.Length);
+                        streamCriptografia.Close();
+                    }
+                    textoCriptografado = Encoding.Unicode.GetString(streamMemoria.ToArray());
+                }
+            }
+            return textoCriptografado;
+        }
+    }
+}
